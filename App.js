c93d55bfa15dc19a4eeb112de981cf6f6a1ed544
Arab45 <Olayiwola>
2025-01-31 14:44:42 -0800

    first commit

--- App.js
+++ App.js
@@ -1,20 +1,27 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View, SafeAreaView, Image } from "react-native";
+import { images } from "./demoImage/splashImage";
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView>
+      <View style={styles.container}>
+        {/* <Text>Open up App.js to start working on your app!</Text> */}
+        <Image source={images.Rectangle} resizeMode="container" style={{colo,  borderWidth: 1, borderColor: 'red', backgroundColor: 'blue'}}/>
+        <StatusBar style="auto" />
+      </View>''
+    </SafeAreaView>
   );
 }
+
+
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    // alignItems: "center",
+    // justifyContent: "center",
+    // fontSize: 8,
+    // fontFamily: "Roboto-Italic-VariableFont_wdth,wght"
   },
 });
